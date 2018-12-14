c65174e3878cbe787214db02bb7f25afc45e8c85
Drakzel <kris501i@easv365.dk>
2018-12-14 20:24:34 +0100

    Ignore
    merge problem fixed.

--- BestinClass.Core/Application Service/Impl/ReviewService.cs
+++ BestinClass.Core/Application Service/Impl/ReviewService.cs
@@ -9,17 +9,18 @@ namespace BestinClass.Core.Application_Service.Impl
 {
     public class ReviewService : IReviewService
     {
+
         private readonly IReviewRepository _reviewRepository;
 
         public ReviewService(IReviewRepository reviewRepository)
         {
             _reviewRepository = reviewRepository;
         }
-        
+
         public Review NewReview(int carId, string header, string body, int ratingEveryday, int ratingWeekend, int ratingPracticality,
             int ratingExterior, int ratingInterior)
         {
-            
+
             var review = new Review()
             {
                 CarId = carId,
@@ -38,13 +39,13 @@ namespace BestinClass.Core.Application_Service.Impl
 
         public Review CreateReview(Review review)
         {
-            if(review.Header.Length < 1)
-                { throw new InvalidDataException("To create review, a header must be attached."); }
-            if(review.Body.Length < 1)
-                { throw new InvalidDataException("To create review, a body must be attached."); }
-            if(review.RatingEveryday < 0 || review.RatingExterior < 0 || review.RatingInterior < 0 ||
+            if (review.Header.Length < 1)
+            { throw new InvalidDataException("To create review, a header must be attached."); }
+            if (review.Body.Length < 1)
+            { throw new InvalidDataException("To create review, a body must be attached."); }
+            if (review.RatingEveryday < 0 || review.RatingExterior < 0 || review.RatingInterior < 0 ||
                 review.RatingOverall < 0 || review.RatingPracticality < 0 || review.RatingWeekend < 0)
-                { throw new InvalidDataException("Review ratings can't be below 0."); }
+            { throw new InvalidDataException("Review ratings can't be below 0."); }
 
             return _reviewRepository.CreateReview(review);
         }
@@ -52,14 +53,14 @@ namespace BestinClass.Core.Application_Service.Impl
         public List<Review> GetAllReviews()
         {
             if (_reviewRepository.ReadAllReviews().Count() < 1)
-                { throw new FileNotFoundException("Database is empty."); }
+            { throw new FileNotFoundException("Database is empty."); }
             return _reviewRepository.ReadAllReviews().ToList();
         }
 
         public Review GetReviewById(int id)
         {
-            if(_reviewRepository.GetReviewById(id) == null)
-                { throw new FileNotFoundException("No match were found."); }
+            if (_reviewRepository.GetReviewById(id) == null)
+            { throw new FileNotFoundException("No match were found."); }
             return _reviewRepository.GetReviewById(id);
         }
 
@@ -71,7 +72,7 @@ namespace BestinClass.Core.Application_Service.Impl
         public void DeleteReview(int id)
         {
             if (_reviewRepository.GetReviewById(id) == null)
-                { throw new FileNotFoundException("No match were found."); }
+            { throw new FileNotFoundException("No match were found."); }
             _reviewRepository.DeleteReview(id);
         }
     }
